c3184504c6e67d4906628ae246cbb9ebd8861654
itggot-adrian-almetunsmeds <adrian.almetunsmeds@elev.ga.ntig.se>
2019-09-29 15:30:30 +0200

    :hammer: Fixed Link#encode method

--- modules/link.rb
+++ modules/link.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: false
+# frozen_string_literal: true
 
 # Public: Encodes a string to be used as a link
 #
@@ -7,10 +7,27 @@
 # Returns a url-encoded string
 #
 class Link
-  def self.encode(link)
-    # FIXME: Whitespace should be replaced with %20
+  # rubocop:disable Metrics/MethodLength
+  def self.encode(link) # rubocop:disable Metrics/AbcSize
+    link = link.downcase
+    i = 0
+    while i < link.length
+      if link[i].ord == 32 # ' '
+        link[i] = '%20'
+      elsif link[i].ord == 228 # 'ä' # rubocop:disable Style/AsciiComments
+        link[i] = '%C3%A4'
+      elsif link[i].ord == 229 # 'å' # rubocop:disable Style/AsciiComments
+        link[i] = '%C3%A5'
+      end
+      i += 1
+    end
+
+    # TODO: Make this process neater.
+    # The entire method is questionable in how it operates
+    link.gsub('\u00E4', '%C3%A4')
     link.gsub('å', '%C3%A5')
     link.gsub('ä', '%C3%A4')
     link.gsub('ö', '%C3%B6')
   end
+  # rubocop:enable Metrics/MethodLength
 end
--- spec/link_spec.rb
+++ spec/link_spec.rb
@@ -1,10 +1,15 @@
-# frozen_string_literal: true
+# frozen_string_literal: false
 
 require 'rspec'
-
 require_relative '../modules/link.rb'
 RSpec.describe 'Link:' do
   it 'Verifies url encoding' do
-    expect(Link.encode('ps öäö s')).to eq 'ps%20%C3%B6%C3%A4%C3%B6%20s'
+    question = 'Kan vi inkludera stora delar'\
+    ' av ett alfabet här eller måste jag dra till med '\
+    'ö'
+    correct = 'kan%20vi%20inkludera%20stora%20delar%20'\
+    'av%20ett%20alfabet%20h%C3%A4r%20eller%20m%C3%A5ste%20jag'\
+    '%20dra%20till%20med%20%C3%B6'
+    expect(Link.encode(question.downcase)).to eq correct
   end
 end
