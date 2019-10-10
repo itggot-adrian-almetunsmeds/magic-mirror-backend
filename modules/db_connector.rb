926333a43bfaaf6f42944eeed42844ae4526a395
itggot-adrian-almetunsmeds <adrian.almetunsmeds@elev.ga.ntig.se>
2019-10-10 21:01:42 +0200

    :sparkles: Added db

---
+++ modules/db_connector.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'sqlite3'
+# Private: Handles connections to the DB
+#
+#
+class DBConnector
+  def initialize
+    connect
+  end
+
+  def self.connect
+    @db ||= SQLite3::Database.new 'db/some.db'
+    @db
+  end
+
+  def self.insert(db, position, value)
+    positions = position[0]
+    position.shift
+    if position.length >= 1
+      position.each do |z|
+        positions += ', ' + z
+      end
+    end
+
+    values = '?'
+    (value.length - 1).times { |_| values += ', ?' }
+    @db.execute("INSERT INTO #{db} (#{positions}) VALUES (#{values})", value[0..value.length])
+  end
+end
