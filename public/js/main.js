e4cb6adf45a63a5f971794d3d7416e8cc303fed3
itggot-adrian-almetunsmeds <adrian.almetunsmeds@elev.ga.ntig.se>
2019-10-29 15:38:56 +0100

    Revert ":hammer: Fixing merge conflicts"
    This reverts commit c82aa7a7021adb05f9b9eeb4da0b735a4ba03d84, reversing
    changes made to 843aaf8bd148cb9590fd2e45530e0d6c7bc3e011.

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,9 @@
-function clock(){
-    let wrapper = document.getElementById("wrapper");
-    
-    let clock = document.createElement("div");
-    clock.classList.add("clock");
-    wrapper.appendChild(clock);
-    let update = setInterval(function(){
-        let time = new Date();
-        let hours = time.getHours();
-        if (hours < 10){
-            hours = ("0"+ hours);
-        }
-        let minutes = time.getMinutes();
-            if (minutes < 10){
-                minutes = ("0"+ minutes);
-            }
-        let seconds = time.getSeconds();
-                if (seconds < 10){
-                    seconds = ("0"+ seconds);
-                }
-        clock.innerHTML = (hours + ":" + minutes + ":" + seconds);
-    }, 500)
-}
 
-clock()
+function Time(){
+    var today = new Data();
+    let time = today.getHours() + ":" + today.getMinutes();
+    let date = today.getDate() + " " + today.getMonth() + " " + today.getYear();
+    let element1 = document.createElement('h2').innerHTML = time
+    let element2 = document.createElement('h3').innerHTML = date
+    
+}
