040dec5c1cd4b9c7bc483a9d86648e85472e0517
itggot-adrian-almetunsmeds <adrian.almetunsmeds@elev.ga.ntig.se>
2019-11-04 19:26:33 +0100

    :sparkles: Added documentation

--- modules/user.rb
+++ modules/user.rb
@@ -3,6 +3,8 @@
 # Public: Used to handle Users
 #
 class User
+  # Checks if the login details are correct and then return the users id
+  # If unable to sign in a error message is returned
   def self.login(username, password)
     @user = DBConnector.connect.execute('SELECT * FROM Users WHERE'\
             ' name=? AND password=?', username, password)
@@ -13,10 +15,15 @@ class User
     end
   end
 
+  # Removes a user from the Users db
+  #
+  # where - String indicating where = ?
+  # equals - String indicating ? = equals
   def self.remove(where, equals)
     DBConnector.remove('Users', where, equals)
   end
 
+  # Checks if there is a user in the db
   def self.excists?
     if all.nil?
       false
@@ -25,10 +32,12 @@ class User
     end
   end
 
+  # Retrieves the users from the db
   def self.all
     DBConnector.connect.execute('SELECT * FROM Users')
   end
 
+  # Adds a new user to the db
   def self.new(name, pass, lang, admin)
     DBConnector.insert('users', %w[name password lang type], [name, pass, lang, admin])
   end
--- modules/websocket.rb
+++ modules/websocket.rb
@@ -2,10 +2,21 @@
 
 # Handles websocket connections
 class Websocket
+  # Parses websocket message
+  #
+  # channel - String
+  # msg - String / Array / Hash
+  #
   def self.parse(channel, msg)
     { channel: channel, message: msg }.to_json
   end
 
+  # Sends the inserted message to the given ws
+  #
+  # websocket - Object
+  # channel - String
+  # msg - String / Array / Hash
+  #
   def self.send(websocket, channel, msg)
     websocket.send(parse(channel, msg))
   end
