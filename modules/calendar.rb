9175acf56a5d78c668ad6197b529c521afa37e19
itggot-adrian-almetunsmeds <adrian.almetunsmeds@elev.ga.ntig.se>
2019-11-11 19:50:36 +0100

    :hammer: Fixed linting

--- modules/calendar.rb
+++ modules/calendar.rb
@@ -5,15 +5,14 @@
 
 # Handles all calendar related tasks
 class Calendar
-    def self.redirect(_)
-      client = Signet::OAuth2::Client.new(
-        client_id: ENV('GOOGLE_API_CLIENT_ID'),
-        client_secret: ENV('GOOGLE_API_CLIENT_SECRET'),
-        authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
-        scope: Google::Apis::CalendarV3::AUTH_CALENDAR_READONLY,
-        redirect_uri: 'http://localhost:9292/google/auth'
-      )
-      redirect client.authorization_uri.to_s
-    end
+  def self.redirect(_)
+    client = Signet::OAuth2::Client.new(
+      client_id: ENV('GOOGLE_API_CLIENT_ID'),
+      client_secret: ENV('GOOGLE_API_CLIENT_SECRET'),
+      authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
+      scope: Google::Apis::CalendarV3::AUTH_CALENDAR_READONLY,
+      redirect_uri: 'http://localhost:9292/google/auth'
+    )
+    redirect client.authorization_uri.to_s
   end
-  
+  end
