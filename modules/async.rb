37f97f8b141dd09f09e6e4be7fbd09bcf23d153a
itggot-adrian-almetunsmeds <adrian.almetunsmeds@elev.ga.ntig.se>
2019-11-16 23:16:18 +0100

    :hammer: Solved not calling update function, still can not call function more than twice

--- modules/async.rb
+++ modules/async.rb
@@ -1,14 +1,19 @@
 # frozen_string_literal: true
 
+require_relative 'websocket'
+
+# Handles async calling
 class Async
-  include SuckerPunch::Job
   # Makes async operation possible
   #
-  # callback - Array (method to be called, method-inputs)
+  # time - Integer (Time in secounds)
+  # object - Object (Object to call method on)
+  # method - String (Name of method to be called)
+  # *args - Values to be passed on as arguments for method call (Max 2, Min 2)
   #
   # Returns nothing
   #
-  def self.perform(callback)
-    callback[0].send(callback[1], callback[2], callback[3])
+  def self.quue(time, object, method, *args)
+    object.send(method, time, args[0], args[1])
   end
 end
--- modules/websocket.rb
+++ modules/websocket.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
-# require 'sucker_punch'
+require 'sucker_punch'
 
 # Handles websocket connections
 class Websocket
   @sockets = []
+
+  include SuckerPunch::Job
 
   # Parses websocket message
   #
@@ -25,27 +27,37 @@ class Websocket
     websocket.send(parse(channel, msg))
   end
 
+  def perform(websocket, user_id)
+    p user_id
+    p 'PERFORMED'
+    x = DBConnector.connect.execute('SELECT * FROM current_sessions WHERE user_id = ?',
+                                    user_id.to_s).first
+    # p x
+    unless x.nil?
+      p 'EXECUTED'
+      Websocket.send_message(websocket, 'weather', [Weather.get(user_id)])
+      Websocket.send_message(websocket, 'traffic', PublicTransport.get(user_id))
+    end
+    Async.quue(5, self, 'update_data', websocket, user_id)
+    # TODO: Running Async.quee resluts in Undefined method
+    #
+    # Running Async.quee calls the method who in turn calls
+    # Websocket.update_data that runs perform_in using Sucket_Punch
+    # but that call results in a
+    # ``` NoMethodError undefined method `update_data' for #<Websocket:0x0000000008496c38> ```
+    # situation.
+  end
+
   # Sends data to clients after it having been updated
   #
+  # time - Integer (Time in secounds until function call)
   # ws - Object (Websocket)
   # user_id - Integer (User id given from session)
   #
   # Returns nothing
   #
-  def self.update_data(websocket, user_id)
-    x = DBConnector.connect.execute('SELECT * FROM current_sessions WHERE user_id = ?',
-                                    user_id.to_s).first
-    unless x.nil?
-      Websocket.send_message(websocket, 'weather', [Weather.get(user_id)])
-      Websocket.send_message(websocket, 'traffic', PublicTransport.get(user_id))
-      Async.perform_in(5, [self, 'update_data', websocket, user_id])
-      # TODO [#15]: Perform_in action can not be run more than once
-      #
-      # The update_data function requires perform that in turn
-      # requires update_data to be defined for it to run.
-      # There by creating a moment 22 situation, where it can only
-      # be run the first time
-    end
+  def self.update_data(time, websocket, user_id)
+    perform_in(time, websocket, user_id)
   end
 
   # Stores the current ws sessions and initiates first data update
@@ -57,7 +69,7 @@ class Websocket
   #
   def self.store(websocket, user_id)
     @sockets << [websocket, user_id]
-    Async.perform_in(5, [self, 'update_data', websocket, user_id])
+    Async.quue(5, self, 'update_data', websocket, user_id)
   end
 
   # Removes a ws session from the stored sessions array
