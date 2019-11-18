180d12f8767bf5800e29e01c72bd355a450b9cf7
itggot-adrian-almetunsmeds <adrian.almetunsmeds@elev.ga.ntig.se>
2019-11-18 18:46:58 +0100

    :tada: Added seeder file for generating db

---
+++ db/seeder.rb
@@ -0,0 +1,109 @@
+# frozen_string_literal: true
+
+require 'sqlite3'
+
+class Seeder
+  def self.seed!
+    db = connect
+    drop_tables(db)
+    puts 'Deleted old tables'
+    create_tables(db)
+    puts 'Created new tables'
+    populate_tables(db)
+    puts 'Populated tables'
+  end
+
+  def self.connect
+    SQLite3::Database.new 'db/configuration.db'
+  end
+
+  def self.drop_tables(db)
+    db.execute('DROP TABLE IF EXISTS ApiKeys;')
+    db.execute('DROP TABLE IF EXISTS Location;')
+    db.execute('DROP TABLE IF EXISTS PublicTransit;')
+    db.execute('DROP TABLE IF EXISTS Users;')
+    db.execute('DROP TABLE IF EXISTS current_sessions;')
+    db.execute('DROP TABLE IF EXISTS transport;')
+    db.execute('DROP TABLE IF EXISTS weather;')
+  end
+
+  def self.create_tables(db)
+    db.execute <<-SQL
+            CREATE TABLE "ApiKeys" (
+                "reseplanerare"  TEXT,
+                "stolptidstabeller" TEXT
+            );
+    SQL
+
+    db.execute <<-SQL
+            CREATE TABLE "location" (
+                "conty"	         TEXT,
+                "lat"            TEXT NOT NULL,
+                "long"           TEXT NOT NULL,
+                "user_id"        INTEGER NOT NULL
+            );
+    SQL
+
+    db.execute <<-SQL
+            CREATE TABLE "PublicTransit" (
+                "Name"             TEXT NOT NULL,
+                "stop_id"          INTEGER NOT NULL,
+                "user_id"          INTEGER NOT NULL
+            );
+    SQL
+
+    db.execute <<-SQL
+            CREATE TABLE "Users" (
+                "id"                INTEGER PRIMARY KEY AUTOINCREMENT,
+                "name"              TEXT NOT NULL,
+                "password"          TEXT NOT NULL,
+                "lang"              TEXT,
+                "type"              Text
+            );
+    SQL
+
+    db.execute <<-SQL
+            CREATE TABLE "current_sessions" (
+                "user_id"               INTEGER
+                );
+    SQL
+
+    db.execute <<-SQL
+            CREATE TABLE "transport" (
+                "user_id"	    INTEGER NOT NULL,
+                "line"	        TEXT NOT NULL,
+                "rtTime"	    TEXT,
+                "time"	        TEXT NOT NULL,
+                "direction"	    TEXT NOT NULL,
+                "operator"	    TEXT,
+                "operatorcode"	TEXT
+            );
+            
+    SQL
+
+    db.execute <<-SQL
+            CREATE TABLE "weather" (
+                "user_id"	    INTEGER NOT NULL,
+                "temp"	        TEXT NOT NULL,
+                "wind_speed"    TEXT NOT NULL,
+                "wind_gust"	    TEXT,
+                "humidity"	    TEXT,
+                "thunder"	    TEXT,
+                "symbol"	    INTEGER NOT NULL,
+                "time"	        TEXT NOT NULL,
+                "pcat"	        TEXT NOT NULL
+            );
+    SQL
+  end
+
+  def self.populate_tables(db)
+
+    users = [
+        {name: 'admin', password: 'admin', lang: 'en', type: 'admin'}
+    ]
+
+    users.each do |d|
+      db.execute('INSERT INTO Users (name, password, lang, type) VALUES (?,?,?,?)', d[:name], d[:password], d[:lang], d[:type])
+    end
+  end
+end
