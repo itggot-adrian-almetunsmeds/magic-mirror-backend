793de6a58f4cb93330c6c8cdda200438ab8914b4
TODO <todo-actions[bot]@users.noreply.github.com>
2019-12-02 20:14:29 +0000

    Update TODO references: #29, #30

--- modules/web_handler.rb
+++ modules/web_handler.rb
@@ -38,7 +38,7 @@ class WebHandler
       i += 1
     end
 
-    # TODO [$5de570a07a85e3000b897fd7]: [#20]: Make this process neater.
+    # TODO [#30]: [#20]: Make this process neater.
     # The entire method is questionable in how it operates
     link.gsub('\u00E4', '%C3%A4')
     link.gsub('å', '%C3%A5')
