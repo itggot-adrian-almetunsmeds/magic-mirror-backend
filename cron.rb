e17157f864bc0824ba8ec7491e29a14392f16c86
itggot-adrian-almetunsmeds <adrian.almetunsmeds@elev.ga.ntig.se>
2020-03-04 19:58:23 +0100

    Transitioned to autoruning cron job. Requires manual start

--- cron.rb
+++ cron.rb
@@ -4,48 +4,90 @@ require_relative 'modules/db_connector'
 require_relative 'modules/public_transport'
 require_relative 'modules/weather'
 require_relative 'modules/calendar'
+Z = DBConnector.connect
+Z.results_as_hash = true
 
-x = DBConnector.connect.execute('SELECT * FROM current_sessions').first
-z = DBConnector.connect
-z.results_as_hash = true
-unless x.nil?
-  # EVERY 1 MINUTE DURING SET TIME INTERVAL
-  x.each do |id|
-    q = z.execute('SELECT * FROM PublicTransit WHERE user_id = ?', id.to_s)
-    q.each do |s|
-      stops = PublicTransport.departures(s['stop_id'])
-      z.execute('DELETE FROM transport WHERE user_id = ?', id)
-      stops.each do |stop|
-        z.execute('INSERT INTO transport (user_id, line, rtTime, time, '\
-          'direction, operator, operatorcode) '\
-        'VALUES (?,?,?,?,?,?,?)', id, stop[0], stop[1], stop[2], stop[3], stop[4], stop[5])
+# EVERY 1 MINUTE
+def update_minute()
+  p "update min"
+  x = DBConnector.connect.execute('SELECT * FROM current_sessions').first
+  unless x.nil?
+    x.each do |id|
+      q = Z.execute('SELECT * FROM PublicTransit WHERE user_id = ?', id.last.to_i)
+      unless q == []  
+        q.each do |s|
+          stops = PublicTransport.departures(s['stop_id'])
+          Z.execute('DELETE FROM transport WHERE user_id = ?', id.last.to_i)
+          stops.each do |stop|
+            unless stop == "No data available"
+              Z.execute('INSERT INTO transport (user_id, line, rtTime, time, '\
+                'direction, operator, operatorcode) '\
+              'VALUES (?,?,?,?,?,?,?)', id.last.to_i, stop[0], stop[1], stop[2], stop[3], stop[4], stop[5])
+              puts "Updated Transit"
+            else
+              puts "Did not update Transit"
+            end
+          end
+        end
+
+      else
+        puts "Did not update Transit"
       end
     end
+
   end
+end
 
-  # EVERY 20 MINUTES
+# EVERY 20 MINUTES
+def update_twenty()
+  p "update twenty"
+  x = DBConnector.connect.execute('SELECT * FROM current_sessions').first
 
-  x.each do |id|
-    begin
-      Calendar.fetch(id)
-      puts 'Updated calendar'
-    rescue StandardError
-      puts 'Unable to update calendar'
-    end
-    k = z.execute('SELECT lat, long FROM Location WHERE user_id = ?', id).first
-    if !k.nil? && !k['lat'].nil? && !k['long'].nil?
-      puts 'Updated weather'
-      w = Weather.current(k['lat'], k['long'])
-      z.execute('DELETE FROM weather WHERE user_id = ?', id)
-      w.each do |weather|
-        z.execute('INSERT INTO weather (user_id, temp, wind_speed, wind_gust, '\
-            'humidity, thunder, symbol, time, pcat)'\
-        ' VALUES (?,?,?,?,?,?,?,?,?)', id, weather[:temp], weather[:wind_speed],
-                  weather[:wind_gust], weather[:humidity],
-                  weather[:thunder], weather[:symbol], weather[:valid_time], weather[:pcat])
+  unless x.nil?
+    x.each do |id|
+      begin
+        Calendar.fetch(id.last.to_i)
+        puts 'Updated calendar'
+      rescue StandardError
+        puts 'Unable to update calendar'
       end
-    else
-      puts 'Did not update weather'
+      begin
+        k = Z.execute('SELECT lat, long FROM Location WHERE user_id = ?', id).first
+        if !k.nil? && !k['lat'].nil? && !k['long'].nil?
+          puts 'Updated weather'
+          w = Weather.current(k['lat'], k['long'])
+          Z.execute('DELETE FROM weather WHERE user_id = ?', id)
+          w.each do |weather|
+            Z.execute('INSERT INTO weather (user_id, temp, wind_speed, wind_gust, '\
+                'humidity, thunder, symbol, time, pcat)'\
+            ' VALUES (?,?,?,?,?,?,?,?,?)', id, weather[:temp], weather[:wind_speed],
+                      weather[:wind_gust], weather[:humidity],
+                      weather[:thunder], weather[:symbol], weather[:valid_time], weather[:pcat])
+          end
+        else
+          puts 'Did not update weather'
+        end
+      rescue
+        puts "Unable to update weather"
+      end
     end
   end
 end
+
+def run()
+  running = true
+  count = 0
+  while running
+    p "started sleeping"
+    sleep 3
+    p "sleept"
+    update_minute()
+    count += 1
+    if count == 1200
+      update_twenty()
+      count = 0
+    end
+  end
+end
+
+run
